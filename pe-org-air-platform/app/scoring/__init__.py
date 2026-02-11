8172f036f9ee729249aae26bdd79c554c9bed52b
Belide-Aakash <belide.a@northeastern.edu>
2026-02-11 02:32:02 -0500

    Implement rubric-based scorer

--- pe-org-air-platform/app/scoring/__init__.py
+++ pe-org-air-platform/app/scoring/__init__.py
@@ -3,6 +3,7 @@ from .evidence_mapper import EvidenceMapper, SIGNAL_TO_DIMENSION_MAP
 from .market_analyzer import PositionFactorCalculator
 from .talent_analyzer import TalentConcentrationCalculator
 from .calculators import VRCalculator, HRCalculator, SynergyCalculator, ConfidenceCalculator
+from .rubric_scorer import RubricScorer, ScoreLevel, RubricResult
 
 __all__ = [
     "EvidenceMapper", 
@@ -12,5 +13,8 @@ __all__ = [
     "VRCalculator",
     "HRCalculator",
     "SynergyCalculator",
-    "ConfidenceCalculator"
+    "ConfidenceCalculator",
+    "RubricScorer",
+    "ScoreLevel",
+    "RubricResult"
 ]
